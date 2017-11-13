707de8e9e793a983f0dc9e5d233cfbd920dfef5c
Vsevolod <oneactiondrama@gmail.com>
2017-11-13 12:56:10 +0300

    first commit

--- PercentageCalculatorTests/PercentageCalculatorTests.swift
+++ PercentageCalculatorTests/PercentageCalculatorTests.swift
@@ -11,26 +11,64 @@ import XCTest
 
 class PercentageCalculatorTests: XCTestCase {
     
+    var vc: ViewController!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        vc = storyboard.instantiateInitialViewController() as! ViewController
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        vc = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPercentageCalculator() {
+        
+        let p = vc.percentage(50, 50)
+        XCTAssert(p == 25)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testLabelValuesShowedProperly() {
+        
+        let _ = vc.view
+        vc.updateLabels(Float(80.0), Float(50.0), Float(40.0))
+        
+        XCTAssert(vc.numberLabel.text == "80.0", "numberLabel doesn't show the right text")
+        XCTAssert(vc.percentageLabel.text == "50.0%", "percentageLabel doesn't show the right text")
+        XCTAssert(vc.resultLabel.text == "40.0", "resultLabel doesn't show the right text")
     }
+    
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
